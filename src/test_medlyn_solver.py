248c1417aa9b3339a95a353ef8675bc041aa6389
Adam Massmann <massmannak@gmail.com>
2017-07-19 11:41:08 -0400

    it appears that the solver is working, now we can start experiments with it

--- src/test_medlyn_solver.py
+++ src/test_medlyn_solver.py
@@ -2,4 +2,32 @@
 """
 This is a module to test if vpd solver is working.
 """
+import codebase.penman_monteith as pm
+from scipy.optimize import fsolve
+import numpy as np
+import metcalcs as met
+import importlib
 
+importlib.reload(pm)
+
+atmos = {}
+atmos['r_n'] = 300. #W/m2
+atmos['rho_a'] = 1.205 #density kg/m3
+atmos['t_a'] = 25. # C
+atmos['rh'] = 70. # rel humdidity
+
+atmos['u_z'] = 2. #wind speed at meas. hiehgt (m/s)
+atmos['vpd'] = met.vapor_pres(atmos['t_a'])*(1.-atmos['rh']/100.)*100.
+atmos['co2'] = 400. # ppm
+
+canopy = {}
+canopy['pft'] = 'DBF'
+canopy['height'] = 10. # plant heigh m
+canopy['lai'] = 1. # leaf area index pierre says 1 max feasible
+
+data_out = fsolve(pm.optimizer_wrapper, 100.,\
+                  full_output=True, args=(atmos, canopy))
+
+print('Medlyn r_l: %f, Oren r_l: %f' % \
+      (atmos['r_s'], pm.oren_r_l(atmos['vpd'], canopy['pft'])))
+# _et = pm.optimizer_wrapper(100.,atmos,canopy)
