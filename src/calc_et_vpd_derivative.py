sign error in LAI,fixed now all debugging should be done

src/calc_et_vpd_derivative.py
@@ -35,7 +35,7 @@ def leaf_vpd(atmos, canopy, lai):
 
 def calc_derivative(atmos, canopy, data):
   """adds various derivative fields to data, given atmos and canopy"""
-  canopy['lai'] = atmos['gamma']*atmos['c_a']*pm.LV\
+  canopy['lai'] = - atmos['gamma']*atmos['c_a']*pm.LV\
                 *np.sqrt(atmos['vpd'])*atmos['p_a']\
                 /(atmos['r_a']\
                   *(data['et_obs']*(atmos['gamma']+atmos['delta'])\

src/plot_et_vpd_derivative.py
@@ -147,7 +147,7 @@ for filename in filenames[:]:
     name = filename.split('/')[-1]
     plt.savefig('%s/climate_et/%s/garb_%s_explicit.png'\
                 % (os.environ['PLOTS'], suff,  name.split('.')[0]))
-    t1 = _df1.loc[np.absolute(_df1.et - _df1.et_obs) < 100., 'et'].copy()
+    t1 = _df1.loc[np.absolute(_df1.et - _df1.et_obs) < 1., 'et'].copy()
     print('for %s, explicit fraction good is %f'\
           % (name, float(t1.count())/float(_df1.et.count())))
 