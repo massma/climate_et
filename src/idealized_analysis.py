27a9941ff4014047ad470bd44a10ed3d221c5430
Adam Massmann <massmannak@gmail.com>
2017-09-11 10:09:05 -0400

    redit analytical formulation and confrimed matches first one

--- src/idealized_analysis.py
+++ src/idealized_analysis.py
@@ -26,46 +26,62 @@ def get_uwue(_df):
   """lazy way to get uwue"""
   return _df.loc[:, ['uwue']].iloc[0]
 
+def d_et(_df):
+  """calcs the full d ET/d Ds, confirmed correct vs df"""
+  return _df['g_a']*_df['p_a']/\
+    ((_df['t_a']+ 273.15)*(_df['gamma']+_df['delta']))*\
+    (pm.CP/_df['r_moist']-_df['gamma']*_df['c_a']*pm.LV/\
+     (_df['lai']*1.6*pm.R_STAR*_df['uwue'])*\
+     (2.*_df['g1']+np.sqrt(_df['vpd']))\
+     /(2.*(_df['g1']+np.sqrt(_df['vpd']))**2))
 
-df['vpd_term'] = (2.*df['g1'] + np.sqrt(df['vpd']))/(\
-                 2.*(df['g1'] + np.sqrt(df['vpd']))**2)
-df['vpd_scale'] = df['gamma']*df['c_a']*pm.LV/\
-                 (1.6*pm.R_STAR*df['uwue'])
-df['lai_term'] = 1./df['lai']
-df['air_term'] = pm.CP/df['r_moist']
-df['scaling'] = df['g_a']*df['p_a']/\
-                ((df['t_a']+273.15)*(df['delta'] + df['gamma']))
-df['d_et_2'] = df['scaling']*(df['air_term']\
-                              -df['vpd_scale']*df['lai_term']*df['vpd_term'])
 
-mean = df.groupby('pft').mean()
-std = df.groupby('pft').std()
 
-def variability(_df):
-  """calcs variability in a comparable way"""
-  out = {}
-  _paren_mean = (_df['air_term']\
-                 -_df['vpd_scale']*df['lai_term']*_df['vpd_term']).mean()
-  _scale_mean = _df['scaling'].mean()
-  out['scale_var'] = _df['scaling'].std()*_paren_mean
-  out['air_var'] = _scale_mean*_df['air_term'].std()
-  out['vpd_term_var'] = _scale_mean*\
-                        (_df['vpd_scale']*df['lai_term']).mean()\
-                        *_df['vpd_term'].std()
-  out['vpd_scale_var'] = _scale_mean*\
-                        _df['vpd_scale'].std()*df['lai_term'].mean()\
-                        *_df['vpd_term'].mean()
-  out['lai_term_var'] = _scale_mean*\
-                        _df['vpd_scale'].mean()*df['lai_term'].std()\
-                        *_df['vpd_term'].mean()
-  out['vpd_var'] = _scale_mean*(_df['vpd_scale']\
-                                *df['lai_term']*_df['vpd_term']).std()
-  out['pft'] = _df['pft'].iloc[0]
-  out = pd.DataFrame(data=out, index=[_df.pft.iloc[0]])
-  return out
+# def partial_p(df):
+#   """calculates partial derivative of d-et/d_Ds w.r.t. P"""
+#   out=
 
-vari = df.groupby('site').apply(variability)
-vari.index = vari.index.droplevel(1)
-test = vari.groupby('pft').mean()
 
-print(test)
+
+# df['vpd_term'] = (2.*df['g1'] + np.sqrt(df['vpd']))/(\
+#                  2.*(df['g1'] + np.sqrt(df['vpd']))**2)
+# df['vpd_scale'] = df['gamma']*df['c_a']*pm.LV/\
+#                  (1.6*pm.R_STAR*df['uwue'])
+# df['lai_term'] = 1./df['lai']
+# df['air_term'] = pm.CP/df['r_moist']
+# df['scaling'] = df['g_a']*df['p_a']/\
+#                 ((df['t_a']+273.15)*(df['delta'] + df['gamma']))
+# df['d_et_2'] = df['scaling']*(df['air_term']\
+#                               -df['vpd_scale']*df['lai_term']*df['vpd_term'])
+
+# mean = df.groupby('pft').mean()
+# std = df.groupby('pft').std()
+
+# def variability(_df):
+#   """calcs variability in a comparable way"""
+#   out = {}
+#   _paren_mean = (_df['air_term']\
+#                  -_df['vpd_scale']*df['lai_term']*_df['vpd_term']).mean()
+#   _scale_mean = _df['scaling'].mean()
+#   out['scale_var'] = _df['scaling'].std()*_paren_mean
+#   out['air_var'] = _scale_mean*_df['air_term'].std()
+#   out['vpd_term_var'] = _scale_mean*\
+#                         (_df['vpd_scale']*df['lai_term']).mean()\
+#                         *_df['vpd_term'].std()
+#   out['vpd_scale_var'] = _scale_mean*\
+#                         _df['vpd_scale'].std()*df['lai_term'].mean()\
+#                         *_df['vpd_term'].mean()
+#   out['lai_term_var'] = _scale_mean*\
+#                         _df['vpd_scale'].mean()*df['lai_term'].std()\
+#                         *_df['vpd_term'].mean()
+#   out['vpd_var'] = _scale_mean*(_df['vpd_scale']\
+#                                 *df['lai_term']*_df['vpd_term']).std()
+#   out['pft'] = _df['pft'].iloc[0]
+#   out = pd.DataFrame(data=out, index=[_df.pft.iloc[0]])
+#   return out
+
+# vari = df.groupby('site').apply(variability)
+# vari.index = vari.index.droplevel(1)
+# test = vari.groupby('pft').mean()
+
+# print(test)
