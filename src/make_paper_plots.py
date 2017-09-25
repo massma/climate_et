09728b3982512026ea9bee9b69d8aadabd0cc525
Adam Massmann <massmannak@gmail.com>
2017-09-25 17:08:53 -0400

    added a map plot, but not sure it really adds anything to the analysis (informationd ensity is low). I htink grouping by pft and doing scatters might be better for paper

--- src/make_paper_plots.py
+++ src/make_paper_plots.py
@@ -21,11 +21,12 @@ df = pd.read_pickle('%s/changjie/full_pandas_seasonal_fit.pkl'\
                     % os.environ['DATA']).loc[:, 'site'].drop_duplicates()
 
 # get metadata
-meta = pd.read_csv('%s/changjie/fluxnet_algorithm/'\
+site_list = pd.read_csv('%s/changjie/fluxnet_algorithm/'\
                    'Site_list_(canopy_height).csv' % (os.environ['DATA']))
 # subset by sites actually used
-meta.index = meta.Site
-meta = meta.loc[df.values]
+site_list.index = site_list.Site
+site_list = site_list.loc[df.values]
+
 
 def make_map(_df):
   """makes a map given a _df with Lat, Lon, and Cover"""
@@ -47,7 +48,7 @@ def make_map(_df):
       print(pft)
       _ds = _df.loc[(_df.Cover_type == pft), ['Latitude', 'Longitude']]
       x, y = m(_ds.Longitude.values, _ds.Latitude.values)
-      ax.scatter(x, y, s=size, label=pft)
+      ax.scatter(x, y, s=size, label=pft, alpha=1.)
     # m.drawparallels(np.arange(-90.,120.,30.))
     # m.drawmeridians(np.arange(0.,420.,60.))
   plt.legend(loc='best')
@@ -55,9 +56,11 @@ def make_map(_df):
   util.test_savefig('../doc/paper/fig01.pdf')
   return
 
+### FIGURE 1 ###
 #make a map fig 1
-make_map(meta)
+make_map(site_list)
 
+### FIGURE 2 ###
 #for final npaper should make this .pdf, and probably
 df = pd.read_pickle('%s/changjie/full_pandas_lai_clean.pkl'\
                     % os.environ['DATA'])
@@ -72,3 +75,40 @@ meta['folder_label'] = ''
 plot_tools.histogram(df, meta)
 os.system('cp %s/climate_et/histogram/full_lai.png '\
           '../doc/paper/fig02.png' % (os.environ['PLOTS']))
+
+def make_quant_map(_df, meta):
+  """makes a map given a _df with Lat, Lon, and Cover"""
+  fig = plt.figure()
+  fig.set_figheight(fig.get_figheight()*4)
+  axs = [fig.add_subplot(4, 1, i+1) for i in range(4)]
+  xlims = [(-140., -50.), (-20., 40.), (10., 40), (110., 155.)]
+  ylims = [(15., 60.), (30., 70.), (-20., -10.), (-40., -10.)]
+  sizes = [24, 12, 40, 40]
+  for ax, xlim, ylim, size in zip(axs, xlims, ylims, sizes):
+    print(ylim)
+    print(xlim)
+    m = Basemap(projection='merc', llcrnrlat=ylim[0], urcrnrlat=ylim[1],\
+            llcrnrlon=xlim[0], urcrnrlon=xlim[1], lat_ts=np.mean(ylims),\
+                resolution='l', ax=ax)#res was l
+    m.drawcoastlines()
+    vmax = 0.07
+    x, y = m(_df.lon.values, _df.lat.values)
+    color = ax.scatter(x, y, c=_df[meta['var']], alpha=0.5,\
+                    s=size, cmap='RdBu',\
+                    vmin=-vmax, vmax=vmax)
+    # m.drawparallels(np.arange(-90.,120.,30.))
+    # m.drawmeridians(np.arange(0.,420.,60.))
+  plt.colorbar(color)
+  plt.tight_layout()
+  util.test_savefig('../doc/paper/fig03.pdf')
+  return
+
+
+### FIGURE 3 ###
+#create figure 2
+meta = {}
+meta['var'] = 'd_et'
+mean_site = df.groupby('site').mean()
+mean_site['lat'] = site_list.loc[mean_site.index, 'Latitude']
+mean_site['lon'] = site_list.loc[mean_site.index, 'Longitude']
+make_quant_map(mean_site, meta)
