checked and just about 100\% sure the scaling has been fixed in all the scripts - calc_tools and calc_vpd* gen script work as they should, and all figs in paper plots have the relelvant quantities doubled

src/codebase/calc_tools.py
@@ -56,30 +56,25 @@ def d_gpp_d_vpd(atmos, canopy):
             *(1.+canopy['g1']/np.sqrt(atmos['vpd']))**2))
   return out
 
-def d_et_d_lai(atmos, canopy):
-  """calc derivative d et/ dlai"""
-  return atmos['g_a']*atmos['p_a']*atmos['gamma']*atmos['c_a']\
-    *np.sqrt(atmos['vpd'])*pm.LV\
-    /(atmos['t_a_k']*(atmos['delta']+atmos['gamma'])*canopy['lai']**2\
-      *pm.R_STAR*1.6*canopy['uwue']*(1. + canopy['g1']/np.sqrt(atmos['vpd'])))
 
-def d_et_d_g_a(atmos, canopy):
+
+def d_et_d_g_a(_df):
   """calc derivative w.r.t. g_a"""
-  return atmos['p_a']/(atmos['t_a_k']*(atmos['delta'] + atmos['gamma']))\
-    *(pm.CP*atmos['vpd']/atmos['r_moist']\
-      -atmos['gamma']*atmos['c_a']*np.sqrt(atmos['vpd'])*pm.LV\
-      /(canopy['lai']*pm.R_STAR*1.6*canopy['uwue']\
-        *(1. + canopy['g1']/np.sqrt(atmos['vpd']))))
+  return _df['p_a']/(_df['t_a_k']*(_df['delta'] + _df['gamma']))\
+    *(pm.CP*_df['vpd']/_df['r_moist']\
+      -_df['gamma']*_df['c_a']*np.sqrt(_df['vpd'])*pm.LV\
+      /(_df['lai']*pm.R_STAR*1.6*_df['uwue']\
+        *(1. + _df['g1']/np.sqrt(_df['vpd']))))
 
-def d_et_d_delta(atmos, canopy):
+def d_et_d_delta(_df):
   """calc derivative w.r.t. delta"""
-  return (atmos['gamma']*(atmos['r_n']-canopy['g_flux'])\
-          -atmos['g_a']*atmos['p_a']/atmos['t_a_k']\
-          *(pm.CP*atmos['vpd']/atmos['r_moist']\
-            -atmos['gamma']*atmos['c_a']*np.sqrt(atmos['vpd'])*pm.LV\
-            /(canopy['lai']*pm.R_STAR*1.6*canopy['uwue']\
-              *(1. + canopy['g1']/np.sqrt(atmos['vpd'])))))\
-              /(atmos['delta'] + atmos['gamma'])**2
+  return (_df['gamma']*(_df['r_n']-_df['g_flux'])\
+          -_df['g_a']*_df['p_a']/_df['t_a_k']\
+          *(pm.CP*_df['vpd']/_df['r_moist']\
+            -_df['gamma']*_df['c_a']*np.sqrt(_df['vpd'])*pm.LV\
+            /(_df['lai']*pm.R_STAR*1.6*_df['uwue']\
+              *(1. + _df['g1']/np.sqrt(_df['vpd'])))))\
+              /(_df['delta'] + _df['gamma'])**2
 
 
 def scaling(atmos):
@@ -87,6 +82,13 @@ def scaling(atmos):
   return 2.*atmos['p_a']/(atmos['r_a']*(273.15+atmos['t_a'])\
                        *(atmos['delta'] + atmos['gamma']))
 
+
+def d_et_d_lai(_df):
+  """calc derivative d et/ dlai"""
+  return _df['g_a']*_df['p_a']*_df['gamma']*_df['c_a']\
+    *np.sqrt(_df['vpd'])*pm.LV\
+    /(_df['t_a_k']*(_df['delta']+_df['gamma'])*_df['lai']**2\
+      *pm.R_STAR*1.6*_df['uwue']*(1. + _df['g1']/np.sqrt(_df['vpd'])))
 
 def calc_derivative(atmos, canopy, data):
   """adds various derivative fields to data, given atmos and canopy"""
@@ -107,9 +109,10 @@ def calc_derivative(atmos, canopy, data):
   data['d_et_vpd_std'] = atmos.vpd.std()*data.d_et # units: W/m2
   data['d_et_vpd_std_leaf'] = atmos.vpd.std()*data.vpd_leaf*data.scaling
   data['d_et_vpd_std_atm'] = atmos.vpd.std()*data.vpd_atm*data.scaling
-  data['d_et_d_lai'] = d_et_d_lai(atmos, canopy)
-  data['d_et_d_g_a'] = d_et_d_g_a(atmos, canopy)
-  data['d_et_d_delta'] = d_et_d_delta(atmos, canopy)
+  concatted = pd.concat([atmos, canopy], axis=1)
+  data['d_et_d_lai'] = d_et_d_lai(concatted)
+  data['d_et_d_g_a'] = d_et_d_g_a(concatted)
+  data['d_et_d_delta'] = d_et_d_delta(concatted)
 
   #Calculate WUE terms
   data['wue_obs'] = data['gpp_obs']/(data['et_obs']/(pm.LV*H2O)*1.e6)
@@ -149,27 +152,3 @@ def d_et_d_r_net(_df):
   """derivantve w.r.t net radiation"""
   return _df['delta']/(_df['delta']+_df['gamma'])
 
-def d_et_d_lai(_df):
-  """calc derivative d et/ dlai"""
-  return _df['g_a']*_df['p_a']*_df['gamma']*_df['c_a']\
-    *np.sqrt(_df['vpd'])*pm.LV\
-    /(_df['t_a_k']*(_df['delta']+_df['gamma'])*_df['lai']**2\
-      *pm.R_STAR*1.6*_df['uwue']*(1. + _df['g1']/np.sqrt(_df['vpd'])))
-
-def d_et_d_g_a(_df):
-  """calc derivative w.r.t. g_a"""
-  return _df['p_a']/(_df['t_a_k']*(_df['delta'] + _df['gamma']))\
-    *(pm.CP*_df['vpd']/_df['r_moist']\
-      -_df['gamma']*_df['c_a']*np.sqrt(_df['vpd'])*pm.LV\
-      /(_df['lai']*pm.R_STAR*1.6*_df['uwue']\
-        *(1. + _df['g1']/np.sqrt(_df['vpd']))))
-
-def d_et_d_delta(_df):
-  """calc derivative w.r.t. delta"""
-  return (_df['gamma']*(_df['r_n']-_df['g_flux'])\
-          -_df['g_a']*_df['p_a']/_df['t_a_k']\
-          *(pm.CP*_df['vpd']/_df['r_moist']\
-            -_df['gamma']*_df['c_a']*np.sqrt(_df['vpd'])*pm.LV\
-            /(_df['lai']*pm.R_STAR*1.6*_df['uwue']\
-              *(1. + _df['g1']/np.sqrt(_df['vpd'])))))\
-              /(_df['delta'] + _df['gamma'])**2

src/paper_figure_code/fig06.py
@@ -68,8 +68,8 @@ def make_ax_plot(_ax, var, _df, meta):
   #                     vmin=vmin, vmax=vmax)
   #below is a hack, should have done by name to begin with
   _df['var'] = var
-  _df = _df.assign(x_cut=pd.cut(_df[meta['x_axis']], 100),\
-                   t_a_cut=pd.cut(_df['t_a'], 100))
+  _df = _df.assign(x_cut=pd.cut(_df[meta['x_axis']], 1000),\
+                   t_a_cut=pd.cut(_df['t_a'], 1000))
   if meta['sample'] == 'sampled':
     grouped = _df.groupby(['x_cut', 't_a_cut']).apply(meshgrid_apply,\
                                                       sample=True)
@@ -91,7 +91,7 @@ def make_ax_plot(_ax, var, _df, meta):
   grouped = grouped.sort_index(axis=1)
   # print('grouped sortex axis 1', grouped)
   grouped = grouped.sort_index(axis=0)
-  # print('grouped sort axis 2', grouped)
+  # print('grouped sort axis 2', grouped)n
   _x = grouped.columns.values
   _y = grouped.index.values
   _x = np.append(_x, _xmax)
@@ -109,10 +109,10 @@ def make_ax_plot(_ax, var, _df, meta):
     t, vpd = vpd_d_et_min(_df)
     _ax.plot(vpd, t, 'k-')
     t_a = np.linspace(_df['t_a'].min(),_df['t_a'].max(), 200.)
-    test = met.vapor_pres(t_a)*100.*(1. - 0.90)
-    _ax.plot(test, t_a, 'k-')
-    test = met.vapor_pres(t_a)*100.*(1. - 0.2)
-    _ax.plot(test, t_a, 'k-')
+    # test = met.vapor_pres(t_a)*100.*(1. - 0.90)
+    # _ax.plot(test, t_a, 'k-')
+    # test = met.vapor_pres(t_a)*100.*(1. - 0.2)
+    # _ax.plot(test, t_a, 'k-')
   _ax.set_xlabel(meta['x_axis'])
   _ax.set_ylabel('T (C)')
   _ax.set_title('PFT: %s; %s'\
@@ -182,7 +182,7 @@ def scatter_plot_paper(_df, meta):
 
   plt.tight_layout()
 
-  fname = '%s/climate_et/paper_plots/scatter/%s_%s.pdf'\
+  fname = '%s/climate_et/paper_plots/scatter/%s_%s.png'\
           % (os.environ['PLOTS'], _df.pft.iloc[0], meta['x_axis'])
 
   try:
@@ -198,19 +198,21 @@ start = time.time()
 plt.close('all')
 meta = {}
 meta['nplots'] = 4 # 5 4
+# if meta['saple'] == sapmpld, then will take a sample over
+# bins instead of average
 # meta['sample'] = 'sampled'
 meta['sample'] = ''
 meta['x_axis'] = 'vpd'
 df.groupby('pft').apply(scatter_plot_paper, meta)
-os.system('convert -append %s/climate_et/paper_plots/scatter/*%s.pdf '\
-          '../../doc/paper/fig06%s.pdf'\
+os.system('convert -append %s/climate_et/paper_plots/scatter/*%s.png '\
+          '../../doc/paper/fig06%s.png'\
           % (os.environ['PLOTS'], meta['x_axis'], meta['sample']))
 print('done with vpd, time was %f min' % ((time.time()-start)/60.))
 
 meta['x_axis'] = 'rh'
 df.groupby('pft').apply(scatter_plot_paper, meta)
-os.system('convert -append %s/climate_et/paper_plots/scatter/*_%s.pdf '\
-          '../../doc/paper/fig06b%s.pdf'\
+os.system('convert -append %s/climate_et/paper_plots/scatter/*_%s.png '\
+          '../../doc/paper/fig06b%s.png'\
           % (os.environ['PLOTS'], meta['x_axis'], meta['sample']))
 print('done with rh, time was %f min' % ((time.time()-start)/60.))
 

src/paper_figure_code/shared_functions.py
@@ -36,6 +36,7 @@ importlib.reload(plot_tools)
 df = pd.read_pickle('%s/changjie/full_pandas_seasonal_fit.pkl'\
                     % os.environ['DATA']).loc[:, 'site'].drop_duplicates()
 
+
 # get metadata
 site_list = pd.read_csv('%s/changjie/fluxnet_algorithm/'\
                    'Site_list_(canopy_height).csv' % (os.environ['DATA']))
@@ -49,12 +50,28 @@ paren_string = r'(Term 2 - Term 3) $\left(\frac{ c_p}{R_{air}} '\
                r'\left( \frac{2 g_1 + \sqrt{D}}'\
                r'{2 (g_1 + \sqrt{D})^2}\right)\right)$'
 
+# below commented df should (and is - I checked) equicalent to fix_scaling_clean
+# df = pd.read_pickle('%s/changjie/full_pandas_lai_clean.pkl'\
+#                     % os.environ['DATA'])
+# # adjustments due to errors found on 10/25
+# df['scaling'] *= 2.
+# df['d_et'] *= 2.
+# df['d_et_vpd_std_leaf'] *= 2.
+# df['d_et_vpd_std'] *= 2.
+# df['d_et_vpd_std_atm'] *= 2.
+# df2 = pd.read_pickle('%s/changjie/full_pandas_fix_scaling_clean.pkl'\
+#                      % os.environ['DATA'])
+# print('old df shape:', df.shape)
+# print('new df shape:', df2.shape)
+# _df = df.apply(pd.to_numeric, errors='coerce')
+# _df2 = df2.apply(pd.to_numeric, errors='coerce')
+# difference = np.nanmax(np.absolute(_df-_df2), axis=0)
+# print(difference.shape)
+# for column1, column2,  _diff in zip(_df.columns, _df2.columns, difference):
+#   print(column1, column2, _diff)
 
-df = pd.read_pickle('%s/changjie/full_pandas_lai_clean.pkl'\
+df = pd.read_pickle('%s/changjie/full_pandas_fix_scaling_clean.pkl'\
                     % os.environ['DATA'])
-# adjustments due to errors found on 10/25
-df['scaling'] *= 2.
-df['d_et'] *= 2.
 
 df['g_a'] = 1./df['r_a']
 df['d_et_leaf'] = df['scaling']*df['vpd_leaf']