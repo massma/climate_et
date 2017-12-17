10fc4bd43251eb2bedb9398a1944cc96575a6b2b
Adam Massmann <massmannak@gmail.com>
2017-12-17 16:50:39 -0500

    making progress'

--- src/analysis.py
+++ src/analysis.py
@@ -14,12 +14,14 @@ df = d_io.load_mat_data()
 df.to_pickle('%s/changjie/loaded_data.pkl' % os.environ['DATA'])
 
 prep_df = d_prep.generate_vars(df)
-calc_df = d_calc.uwue(prep_df)
+calc_df = d_calc.all_diagnostics(prep_df)
 
 def max_diff(quant1, quant2):
   """caclualte the maximuma boslute difference"""
   return np.nanmax(np.absolute(quant1-quant2))
 
-importlib.reload(calc_df)
+importlib.reload(test)
 test.test_et_model(calc_df)
-#is uwue goo
+
+
+
--- src/codebase/data_calc.py
+++ src/codebase/data_calc.py
@@ -1,5 +1,8 @@
+#! ~/edward/bin/python
 
-
+"""
+This module calculates all diagnostics used in the paper
+"""
 import numpy as np
 
 #geophysical constats
@@ -25,4 +28,65 @@ def uwue(_df):
                    -_df['delta']*_df['r_net']\
                    -_df['g_a']*_df['rho_a']*CP*_df['vpd'])\
                   *1.6*R_STAR*_df['t_a_k']*(1.0+_df['g1']/np.sqrt(_df['vpd'])))
+  return _df
+
+def clean_df(_df):
+  """
+  cleans df, for now removing lower and upper 5 percent of values,
+  based on calc'd uwue. Could look into more sophisticated outlier
+  identification (seems like from histogram there are clear ouliers).
+  """
+  clean_df = _df.loc[((_df.uwue < _df.uwue.quantile(q=0.95)) &\
+                      (_df.uwue > _df.uwue.quantile(q=0.05)))]
+  return clean_df
+
+def pm_et(_df, vpd=None):
+  """calculates et using our new penman-monteith"""
+  if vpd is None:
+    vpd = _df['vpd']
+  _df['et'] = (_df['delta']*_df['r_net']\
+               +_df['g_a']*_df['p_a']/_df['t_a_k']\
+               *(CP*vpd/_df['r_moist']\
+                 -_df['gamma']*_df['c_a']*np.sqrt(vpd)\
+                 /(R_STAR*1.6*_df['uwue']\
+                   *(1.0+_df['g1']/np.sqrt(vpd)))))\
+                   /(_df['delta']+_df['gamma'])
+  return _df
+
+def sign(_df, vpd=None):
+  """calculates the 'sign' term"""
+  if vpd is None:
+    vpd = _df['vpd']
+  _df['sign'] = CP/_df['r_moist']\
+                -_df['gamma']*_df['c_a']\
+                /(1.6*R_STAR*_df['uwue'])\
+                *((2.*_df['g1']+np.sqrt(vpd))\
+                  /(2.0*(_df['g1']+np.sqrt(vpd))**2))
+  return _df
+
+def scaling(_df):
+  """
+  calculates the 'scaling' term,
+  note 2.0 factor is from esat-rh decomposition,
+  but need to double check this
+  """
+  _df['scaling'] = 2.0*_df['g_a']*_df['p_a']\
+                   /(_df['t_a_k']*(_df['delta'] + _df['gamma']))
+  return _df
+
+def d_et_d_vpd(_df, vpd=None):
+  """calculates the derivative of et w.r.t. vpd"""
+  if vpd is None:
+    vpd = _df['vpd']
+  _df = scaling(_df)
+  _df = sign(_df)
+  _df['d_et'] = _df['scaling']*_df['sign']
+  return _df
+
+def all_diagnostics(_df):
+  """calcualtes all diagnostics"""
+  _df = uwue(_df)
+  _df = clean_df(_df)
+  _df = pm_et(_df)
+  _df = d_et_d_vpd(_df)
   return _df
