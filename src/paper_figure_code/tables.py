f506347a0cf7d746b715a69fd5440e0c8eb6f83f
Adam Massmann <massmannak@gmail.com>
2018-02-14 17:20:09 -0500

    done with pierre's comments, still ahve some q's (mostly with citations), and then just clean up, format to Glob Change Bio, and submit

--- src/paper_figure_code/tables.py
+++ src/paper_figure_code/tables.py
@@ -42,98 +42,98 @@ test = mean_df.loc[:, ['d_et', 'd_et_bar', 'd_et_bar_std',\
 print(test)
 
 # think below is jsut for reference for the series, but not used in paper
-columns = []
-for i in range(1, 5):
-  columns.append('term_%d' % i)
-  mean_df[columns[-1]] = 1./(mean_df.lai*mean_df.uwue_norm*mean_df.g1**i)
+# columns = []
+# for i in range(1, 5):
+#   columns.append('term_%d' % i)
+#   mean_df[columns[-1]] = 1./(mean_df.lai*mean_df.uwue_norm*mean_df.g1**i)
 
-print(mean_df.loc[:, columns])
+# print(mean_df.loc[:, columns])
 
-mean_df['vpd_crit'] = et_min_vpd(mean_df, mean_df.lai)
-mean_df['sigma_dot_uwue'] = mean_df.lai*mean_df.uwue_norm
-columns = ['r_moist', 'c_a', 'gamma', 'lai',\
-           'sigma_dot_uwue', 'vpd_crit']
-print('\n vpd critical table')
-print(mean_df.loc[:, columns])
+# mean_df['vpd_crit'] = et_min_vpd(mean_df, mean_df.lai)
+# mean_df['sigma_dot_uwue'] = mean_df.lai*mean_df.uwue_norm
+# columns = ['r_moist', 'c_a', 'gamma', 'lai',\
+#            'sigma_dot_uwue', 'vpd_crit']
+# print('\n vpd critical table')
+# print(mean_df.loc[:, columns])
 
-def optimizer(vpds, *args):
-  """this finds the otopmism vpd to get the most true hits"""
-  _dff = args[0]
-  count_true_less = np.array([float(_dff.loc[((_dff.vpd < vpd)\
-                                    & (_dff.d_et < 0.0)), 'd_et'].count())\
-                              for vpd in vpds])
-  count_true_more = np.array([float(_dff.loc[((_dff.vpd > vpd)\
-                                    & (_dff.d_et > 0.0)), 'd_et'].count())\
-                              for vpd in vpds])
-  output = (count_true_less+count_true_more)#/float(_dff.d_et.count())
-  return output
+# def optimizer(vpds, *args):
+#   """this finds the otopmism vpd to get the most true hits"""
+#   _dff = args[0]
+#   count_true_less = np.array([float(_dff.loc[((_dff.vpd < vpd)\
+#                                     & (_dff.d_et < 0.0)), 'd_et'].count())\
+#                               for vpd in vpds])
+#   count_true_more = np.array([float(_dff.loc[((_dff.vpd > vpd)\
+#                                     & (_dff.d_et > 0.0)), 'd_et'].count())\
+#                               for vpd in vpds])
+#   output = (count_true_less+count_true_more)#/float(_dff.d_et.count())
+#   return output
 
 
-def vpd_statistics(_df, ax, site=False):
-  """computes fraction of time theory is correct, and mean det/dvpd as a
-  function of theory"""
-  columns = ['frac_correct_less', 'frac_incorrect_less',\
-             'frac_correct_more', 'frac_incorrect_more',\
-             'total_frac_correct', 'mean_less', 'mean_more', 'mean'] # ,\
-             # 'optimized_vpd', 'true_vpd']
-  # mean_df.loc[_df.pft.iloc[0], 'vpd_crit']
-  # median = _df.median()
-  vpd_crit = et_min_vpd(_df.mean(), _df.mean().lai)#median.lai)
-  print(vpd_crit)
-  less_df = _df.d_et.loc[(_df.vpd < vpd_crit)]
-  try:
-    less = [float(less_df.loc[less_df < 0.0].count())/float(less_df.count()),
-            float(less_df.loc[less_df > 0.0].count())/float(less_df.count())]
-  except ZeroDivisionError:
-    less = [0.0, 0.0]
+# def vpd_statistics(_df, ax, site=False):
+#   """computes fraction of time theory is correct, and mean det/dvpd as a
+#   function of theory"""
+#   columns = ['frac_correct_less', 'frac_incorrect_less',\
+#              'frac_correct_more', 'frac_incorrect_more',\
+#              'total_frac_correct', 'mean_less', 'mean_more', 'mean'] # ,\
+#              # 'optimized_vpd', 'true_vpd']
+#   # mean_df.loc[_df.pft.iloc[0], 'vpd_crit']
+#   # median = _df.median()
+#   vpd_crit = et_min_vpd(_df.mean(), _df.mean().lai)#median.lai)
+#   print(vpd_crit)
+#   less_df = _df.d_et.loc[(_df.vpd < vpd_crit)]
+#   try:
+#     less = [float(less_df.loc[less_df < 0.0].count())/float(less_df.count()),
+#             float(less_df.loc[less_df > 0.0].count())/float(less_df.count())]
+#   except ZeroDivisionError:
+#     less = [0.0, 0.0]
 
-  more_df = _df.d_et.loc[(_df.vpd > vpd_crit)]
-  try:
-    more = [float(more_df.loc[more_df > 0.0].count())/float(more_df.count()),
-            float(more_df.loc[more_df < 0.0].count())/float(more_df.count())]
-  except ZeroDivisionError:
-    more = [0.0, 0.0]
-  # print('test', vpd_crit)
-  # vpd_opt = scipy.optimize.golden(optimizer, args=(_df))# ,\
-  #                                 # brack=(0.0, 5.0*vpd_crit),\
-  # print(vpd_opt)
-  vpds = np.linspace(0.0, _df.vpd.max())
-  hits = optimizer(vpds, _df)
-  if site:
-    p = ax.plot(vpds, hits, label=_df.site.iloc[0])
-    ax.set_title(_df.pft.iloc[0])
-  else:
-    p = ax.plot(vpds, hits, label=_df.pft.iloc[0])
-  ax.plot([vpd_crit], [hits[np.absolute(vpds-vpd_crit).argmin()]],\
-          marker='*', color=p[-1].get_color())
-  #ax.plot([vpd_crit, vpd_crit], [hits.min(), hits.max()], label=_df.pft.iloc[0])
+#   more_df = _df.d_et.loc[(_df.vpd > vpd_crit)]
+#   try:
+#     more = [float(more_df.loc[more_df > 0.0].count())/float(more_df.count()),
+#             float(more_df.loc[more_df < 0.0].count())/float(more_df.count())]
+#   except ZeroDivisionError:
+#     more = [0.0, 0.0]
+#   # print('test', vpd_crit)
+#   # vpd_opt = scipy.optimize.golden(optimizer, args=(_df))# ,\
+#   #                                 # brack=(0.0, 5.0*vpd_crit),\
+#   # print(vpd_opt)
+#   vpds = np.linspace(0.0, _df.vpd.max())
+#   hits = optimizer(vpds, _df)
+#   if site:
+#     p = ax.plot(vpds, hits, label=_df.site.iloc[0])
+#     ax.set_title(_df.pft.iloc[0])
+#   else:
+#     p = ax.plot(vpds, hits, label=_df.pft.iloc[0])
+#   ax.plot([vpd_crit], [hits[np.absolute(vpds-vpd_crit).argmin()]],\
+#           marker='*', color=p[-1].get_color())
+#   #ax.plot([vpd_crit, vpd_crit], [hits.min(), hits.max()], label=_df.pft.iloc[0])
 
-  data = np.hstack([less, more,\
-                    [float(optimizer([vpd_crit], _df))/_df.d_et.count(),\
-                     less_df.mean(), more_df.mean() , _df.d_et.mean()]])
-  #, vpd_opt, vpd_crit]
-  df_out = pd.DataFrame(data=[data], index=[_df.pft.iloc[0]], columns=columns)
-  return df_out
+#   data = np.hstack([less, more,\
+#                     [float(optimizer([vpd_crit], _df))/_df.d_et.count(),\
+#                      less_df.mean(), more_df.mean() , _df.d_et.mean()]])
+#   #, vpd_opt, vpd_crit]
+#   df_out = pd.DataFrame(data=[data], index=[_df.pft.iloc[0]], columns=columns)
+#   return df_out
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-fraction_data = df.groupby('pft').apply(vpd_statistics, ax)
-plt.legend(loc='best')
-ax.set_xlabel('VPD (Pa)')
-ax.set_ylabel('# observations theory gets right')
-plt.savefig('./for_agu.pdf')
-print(fraction_data)
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# fraction_data = df.groupby('pft').apply(vpd_statistics, ax)
+# plt.legend(loc='best')
+# ax.set_xlabel('VPD (Pa)')
+# ax.set_ylabel('# observations theory gets right')
+# plt.savefig('./for_agu.pdf')
+# print(fraction_data)
 
-def site_plot(pft):
-  """just a wrapper for doing indiviual site plots"""
-  fig = plt.figure()
-  ax = fig.add_subplot(111)
-  fraction_data = df.loc[(df.pft == pft), :]\
-                    .groupby('site').apply(vpd_statistics, ax, site=True)
-  plt.legend(loc='best')
-  plt.savefig('./garb_%s_normalized.png' % pft)
-  print(fraction_data)
-  return
+# def site_plot(pft):
+#   """just a wrapper for doing indiviual site plots"""
+#   fig = plt.figure()
+#   ax = fig.add_subplot(111)
+#   fraction_data = df.loc[(df.pft == pft), :]\
+#                     .groupby('site').apply(vpd_statistics, ax, site=True)
+#   plt.legend(loc='best')
+#   plt.savefig('./garb_%s_normalized.png' % pft)
+#   print(fraction_data)
+#   return
 
 # site_plot('DBF')
 # site_plot('ENF')
